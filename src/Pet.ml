ed1e565cb8a96f14311e29f0139aa3cdccc294e5
Vincent Jacquier <vin.jacquier@gmail.com>
2015-06-26 21:17:18 +0200

    add class pet (pet.ml)

--- src/Pet.ml
+++ src/Pet.ml
@@ -9,3 +9,37 @@
 (*   Created: 2015/06/26 by vjacquie jmoiroux         ###   ########.fr      *)
 (*                                                                           *)
 (*****************************************************************************)
+
+class Pet =
+	object (self)
+		val mutable _health = 100
+		val mutable _energy = 100
+		val mutable _hygiene = 100
+		val mutable _happyness = 100
+
+		method eat =
+			_health <- _health + 20;
+			_energy <- _energy - 10;
+			_hygiene <- _hygiene - 20;
+			_happyness <- _happyness + 5
+
+		method thunder =
+			_health <- _health - 20;
+			_energy <- _energy + 25;
+			_happyness <- _happyness - 20
+
+		method bath =
+			_health <- _health - 20;
+			_energy <- _energy - 10;
+			_hygiene <- _hygiene + 25;
+			_happyness <- _happyness + 5
+
+		method kill =
+			_health <- _health - 20;
+			_energy <- _energy - 10;
+			_happyness <- _happyness + 20
+
+		method is_alive =
+			if _health = 0 || _energy = 0 || _hygiene = 0 || _happyness = 0 then false
+			else true
+	end
