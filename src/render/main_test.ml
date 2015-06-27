c8b954403c03fb1948af7ef925237580a201adce
Jean-Jacques MOIROUX <jmoiroux@student.42.fr>
2015-06-27 14:00:10 +0200

    update, il faut trouver coment mettre une image et c finit

--- src/render/main_test.ml
+++ src/render/main_test.ml
@@ -5,27 +5,26 @@ class render =
 		val win_x = 1280
 		val win_y = 720
 		val win_title = "Tamagotchi by jmoiroux & vjacquie"
-
 		
 		method loadButtons vbox =
-			let button1 = GButton.button ~label:"EAT" ~packing:vbox#add () in
-			let button2 = GButton.button ~label:"THUNDER" ~packing:vbox#add () in
-			let button3 = GButton.button ~label:"BATH" ~packing:vbox#add () in
-			let button4 = GButton.button ~label:"KILL" ~packing:vbox#add () in
-			ignore(button1#connect#clicked ~callback: (fun () -> prerr_endline "EAT"));
-			ignore(button2#connect#clicked ~callback: (fun () -> prerr_endline "THUNDER"));
-			ignore(button3#connect#clicked ~callback: (fun () -> prerr_endline "BATH"));
-			ignore(button4#connect#clicked ~callback: (fun () -> prerr_endline "KILL"));
+			let button1 = GButton.button ~label:"EAT Function" ~packing:vbox#add () in
+			let button2 = GButton.button ~label:"THUNDER Function" ~packing:vbox#add () in
+			let button3 = GButton.button ~label:"BATH Function" ~packing:vbox#add () in
+			let button4 = GButton.button ~label:"KILL Function" ~packing:vbox#add () in
+			ignore(button1#connect#clicked ~callback: (fun () -> prerr_endline "EAT Function"));
+			ignore(button2#connect#clicked ~callback: (fun () -> prerr_endline "THUNDER Function"));
+			ignore(button3#connect#clicked ~callback: (fun () -> prerr_endline "BATH Function"));
+			ignore(button4#connect#clicked ~callback: (fun () -> prerr_endline "KILL Function"));
 
 		method loadButtonsTop vbox =
-			let button1 = GButton.button ~label:"health" ~packing:vbox#add () in
-			let button2 = GButton.button ~label:"energy" ~packing:vbox#add () in
-			let button3 = GButton.button ~label:"hygiene" ~packing:vbox#add () in
-			let button4 = GButton.button ~label:"happyness" ~packing:vbox#add () in
-			ignore(button1#connect#clicked ~callback: (fun () -> prerr_endline "health"));
-			ignore(button2#connect#clicked ~callback: (fun () -> prerr_endline "energy"));
-			ignore(button3#connect#clicked ~callback: (fun () -> prerr_endline "hygiene"));
-			ignore(button4#connect#clicked ~callback: (fun () -> prerr_endline "happyness"))
+			let button1 = GButton.button ~label:"Health Status" ~packing:vbox#add () in
+			let button2 = GButton.button ~label:"Energy Status" ~packing:vbox#add () in
+			let button3 = GButton.button ~label:"Hygiene Status" ~packing:vbox#add () in
+			let button4 = GButton.button ~label:"Happyness Status" ~packing:vbox#add () in
+			ignore(button1#connect#clicked ~callback: (fun () -> prerr_endline "health Status"));
+			ignore(button2#connect#clicked ~callback: (fun () -> prerr_endline "energy Status"));
+			ignore(button3#connect#clicked ~callback: (fun () -> prerr_endline "hygiene Status"));
+			ignore(button4#connect#clicked ~callback: (fun () -> prerr_endline "happyness Status"))
 
 
 		method init =
@@ -36,16 +35,15 @@ class render =
 										~resizable:false () in
 
 
-			let footer = GPack.vbox ~width:500 ~height:100 ~packing:window#add ~show:true () in
-			let head = GPack.vbox ~width:500 ~height:100 ~packing:footer#add ~show:true () in
-			let image = GMisc.drawing_area ~width:500 ~height:400 ~packing:footer#add ~show:true () in
+			let mainContainer = GPack.vbox ~width:500 ~height:100 ~packing:window#add ~show:true () in
+			let footer = GPack.vbox ~width:500 ~height:100 ~packing:mainContainer#add ~show:true () in
+			let image = GMisc.drawing_area ~width:500 ~height:400 ~packing:mainContainer#add ~show:true () in
+			let head = GPack.vbox ~width:500 ~height:100 ~packing:mainContainer#add ~show:true () in
 			
 
 			ignore(window#connect#destroy ~callback:GMain.Main.quit);
 
-
-
-			let menubar1 = GMenu.menu_bar ~packing:head#pack () in
+			let menubar1 = GMenu.menu_bar ~packing:footer#pack () in
 			let factory1 = new GMenu.factory menubar1 in
 			let accel_group = factory1#accel_group in
 			let file_menu = factory1#add_submenu "File" in
@@ -54,9 +52,8 @@ class render =
 			ignore(factory#add_item "Quit" ~key:GdkKeysyms._Q ~callback: GMain.Main.quit);
 
 
-			self#loadButtonsTop head;
-			self#loadButtons footer;
-			
+			self#loadButtonsTop footer;
+			self#loadButtons head;			
 			(* Display the windows and enter Gtk+ main loop *)
 			window#add_accel_group accel_group;
 			window#show ();
